d5d9d12232ca94a77acd36e3911a82719a2c5998
bwestbrook <jamin.westbrook@gmail.com>
2025-01-09 19:13:51 -0800

    disable Host CHeck in devServer allowedHosts: 'all'

--- vue.config.js
+++ vue.config.js
@@ -5,8 +5,7 @@ module.exports = defineConfig({
     port: 3000,
     hot: true, // Enable Hot Module Replacement
     liveReload: true, // Enable live reloading
-    disableHostCheck: true
-    
+    allowedHosts: 'all'
     },
   configureWebpack: {
    
