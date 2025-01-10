add533d1e7e0f11c5c63ca05836f1669a9875ede
bwestbrook <jamin.westbrook@gmail.com>
2025-01-09 18:08:19 -0800

    source vrom vue create

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <mainBody :wallet="wallet" :socket="socket"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { BeaconWallet } from '@taquito/beacon-wallet'
+import { NetworkType } from "@airgap/beacon-types";
+import io from 'socket.io-client'
+import mainBody from "./components/mainBody.vue"
+//import { PORT } from '../constants.js'
+
+let globalWallet = undefined
+//let wallet = undefined
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    mainBody
+  },
+  data () {
+    return {
+      wallet: this.wallet,
+      socket: this.socket
+    }
+  },
+  methods: {
+    async getWallet() {
+      if (!globalWallet) {
+          // Create a new BeaconWallet instance. The options will be passed to the DAppClient constructor.
+          const wallet = new BeaconWallet({ 
+              name: 'TezTacToe', network: {
+              type: NetworkType.GHOSTNET
+            }
+          })   
+          globalWallet = wallet
+          this.wallet = globalWallet
+        }
+      return {        
+        wallet: globalWallet
+      }
+    }
+  },
+  created() {    
+    this.socket = io("http://127.0.0.1:3000")
+    this.getWallet()
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,28 @@
 const { defineConfig } = require('@vue/cli-service')
+var webpack = require('webpack');
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  devServer: {
+    port: 3000,
+    hot: true, // Enable Hot Module Replacement
+    liveReload: true, // Enable live reloading
+    
+    },
+  configureWebpack: {
+   
+      resolve: {
+        fallback: {
+          path: require.resolve("path-browserify"),
+          stream: require.resolve("stream-browserify"),
+          buffer: require.resolve("buffer"),
+          crypto: false,
+        },
+      },
+      plugins: [
+        new webpack.ProvidePlugin({
+          Buffer:  ["buffer",  "Buffer"]
+        })
+      ]
+    },
+  }
+)
+
