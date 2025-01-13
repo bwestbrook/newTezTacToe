df80d8c81de61e3f15700ecab02737ba01f88089
bwestbrook <jamin.westbrook@gmail.com>
2025-01-13 02:11:08 -0800

    herok sever

--- src/App.vue
+++ src/App.vue
@@ -85,7 +85,7 @@ export default {
   },
   created() {    
 
-      this.socket = io("https://damp-spire-29654-cc0ffbb43258.herokuapp.com:3000")
+      this.socket = io("https://damp-spire-29654-cc0ffbb43258.herokuapp.com")
     
       console.log('######################')
       console.log('######################')
