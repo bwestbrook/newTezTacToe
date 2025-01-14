9e368025ed53f547ae03535b8acfafbf7a72e7f8
bwestbrook <jamin.westbrook@gmail.com>
2025-01-13 16:54:07 -0800

    new dist

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const app = express()
 const cors = require("cors");
 const http = require('http')
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'dist')))
+//app.set('views', path.join(__dirname, 'views'))
+//app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
   console.log(`Rendering 'pages/index' for route '/'`)
