e31df8ecbf405339724e2ead4d38ac65b2f0fbcb
bwestbrook <jamin.westbrook@gmail.com>
2025-01-27 16:33:41 -0800

    quick push

--- src/components/mainBody.vue
+++ src/components/mainBody.vue
@@ -103,7 +103,7 @@ export default {
   margin:5px;
   padding:1px;
   max-width: 610px;
-  font-family: 'Noto Serif';
+  font-family: 'Arimo';
   display: flex;
   flex-direction: column;
   margin: auto;
